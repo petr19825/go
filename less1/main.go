6f8fd90a460a879c2596df583c5443edebf76d8a
petr <petr19825@yandex.ru>
2024-03-11 11:49:12 +0700

    less2

--- less1/main.go
+++ less1/main.go
@@ -2,6 +2,23 @@ package main
 
 import "fmt"
 
+var age = 35
+var a, b = 35, "petr"
+
+func test(chis int, st int) int {
+	temp := chis
+
+	for i := 1; i < st; i++ {
+		chis = chis * temp
+	}
+	return chis
+}
+
 func main() {
 	fmt.Println("Hello world")
+	fmt.Println("Second line")
+	fmt.Printf("Привет %s %d %.3f\n", "Пётр", age, 2.5745)
+	fmt.Printf("Привет %T \n", age)
+	fmt.Printf("%d %s\n", a, b)
+	fmt.Printf("%d\n", test(2, 3))
 }
